881dc1613dd5954c7b285fa481dda84167daf967
youjin <w2117@e-mirim.hs.kr>
2023-06-01 10:41:14 +0900

    경로 저장 완료

--- app.js
+++ app.js
@@ -1,6 +1,78 @@
+// const express = require('express');
+// const mysql = require('mysql');
+// const cors = require('cors');
+
+// const app = express();
+// const port = 3001;
+
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb', extended: true }));
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   })
+// );
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: '1234',
+//   database: 'frameme',
+// });
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error('mysql connection error');
+//     console.error(err);
+//     throw err;
+//   } else {
+//     console.log('연결에 성공하였습니다.');
+//   }
+// });
+
+// app.post('/upload', async (req, res) => {
+//   try {
+//     const { photos } = req.body;
+
+//     // 이미지 경로를 image1, image2, image3, image4로 변환하여 MySQL 데이터베이스에 삽입
+//     for (let i = 0; i < photos.length; i += 4) {
+//       const image1 = `image${i + 1}`;
+//       const image2 = `image${i + 2}`;
+//       const image3 = `image${i + 3}`;
+//       const image4 = `image${i + 4}`;
+
+//       // 사진을 Blob 형태로 변환
+//       const blobImage1 = Buffer.from(photos[i], 'base64');
+//       const blobImage2 = Buffer.from(photos[i + 1], 'base64');
+//       const blobImage3 = Buffer.from(photos[i + 2], 'base64');
+//       const blobImage4 = Buffer.from(photos[i + 3], 'base64');
+
+//       // MySQL 데이터베이스에 삽입하는 쿼리 실행
+//       await connection.query(
+//         'INSERT INTO photos (image1, image2, image3, image4) VALUES (?, ?, ?, ?)',
+//         [blobImage1, blobImage2, blobImage3, blobImage4]
+//       );
+//     }
+
+//     res.status(200).json({ message: 'Photos uploaded successfully' });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Error uploading photos' });
+//   }
+// });
+
+// // 서버 시작
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3001;
@@ -32,27 +104,33 @@ connection.connect(function (err) {
   }
 });
 
+// uploads 폴더가 없을 경우 생성
+const uploadsFolder = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsFolder)) {
+  fs.mkdirSync(uploadsFolder);
+}
+
 app.post('/upload', async (req, res) => {
   try {
     const { photos } = req.body;
 
-    // 이미지 경로를 image1, image2, image3, image4로 변환하여 MySQL 데이터베이스에 삽입
+    // 이미지 파일을 서버에 업로드하고 경로를 데이터베이스에 삽입
     for (let i = 0; i < photos.length; i += 4) {
       const image1 = `image${i + 1}`;
       const image2 = `image${i + 2}`;
       const image3 = `image${i + 3}`;
       const image4 = `image${i + 4}`;
 
-      // 사진을 Blob 형태로 변환
-      const blobImage1 = Buffer.from(photos[i], 'base64');
-      const blobImage2 = Buffer.from(photos[i + 1], 'base64');
-      const blobImage3 = Buffer.from(photos[i + 2], 'base64');
-      const blobImage4 = Buffer.from(photos[i + 3], 'base64');
+      // 이미지 파일을 서버의 파일 시스템에 저장
+      const image1Path = saveImage(photos[i]);
+      const image2Path = saveImage(photos[i + 1]);
+      const image3Path = saveImage(photos[i + 2]);
+      const image4Path = saveImage(photos[i + 3]);
 
-      // MySQL 데이터베이스에 삽입하는 쿼리 실행
+      // 데이터베이스에 이미지 파일의 경로 삽입
       await connection.query(
         'INSERT INTO photos (image1, image2, image3, image4) VALUES (?, ?, ?, ?)',
-        [blobImage1, blobImage2, blobImage3, blobImage4]
+        [image1Path, image2Path, image3Path, image4Path]
       );
     }
 
@@ -62,6 +140,18 @@ app.post('/upload', async (req, res) => {
     res.status(500).json({ message: 'Error uploading photos' });
   }
 });
+
+// 이미지 파일을 서버의 파일 시스템에 저장하는 함수
+function saveImage(base64Data) {
+  const fileName = `${Date.now()}.png`;
+  const filePath = path.join(uploadsFolder, fileName);
+
+  // base64 데이터를 파일로 변환하여 저장
+  fs.writeFileSync(filePath, base64Data, 'base64');
+
+  // 저장된 파일의 경로 반환
+  return filePath;
+}
 
 // 서버 시작
 app.listen(port, () => {
