8acce6e73e3bbffd6eed64887874725771c057c2
Kim-yujin <w2117@e-mirim.hs.kr>
2023-06-13 07:11:17 +0900

    server

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const multer = require('multer');
 const QRCode = require('qrcode');
 const fs = require('fs');
-const mysql = require('mysql2');
-const config = require('./config');
 
 const upload = multer({
     limits: {
@@ -23,8 +21,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
-
-const connection = mysql.createConnection(config);
 
 app.post('/upload', upload.single('image'), async(req, res) => {
     try {
@@ -69,44 +65,6 @@ app.get('/download/:filename', (req, res) => {
     });
 });
 
-// 데이터 삽입 
-app.post('/api/insert', (req, res) => {
-    // 클라이언트로부터 전달받은 사용자 정보
-    const { name, today, time, qrCodeImage, frameimage } = req.body;
-
-    // TODO: 데이터베이스에 데이터 삽입 로직 구현
-    const query = 'INSERT INTO gallery (name, day, time, qr, frame) VALUES (?, ?, ?, ?, ?)';
-    connection.query(query, [name, today, time, qrCodeImage, frameimage], (error, results) => {
-        if (error) {
-            console.error('삽입 중 오류 발생:', error);
-            res.status(500).json({ error: '삽입에 실패했습니다.' });
-        }
-        console.log('성공:', results);
-        res.json({ message: '성공적으로 완료되었습니다.' });
-    });
-});
-
-// 데이터 조회
-app.get('/api/select', (req, res) => {
-    // 실제 로그인 검증 로직
-    const query = ' select * from gallery'
-    connection.query(query, (err, results) => {
-        if (err) {
-            console.error('데이터베이스 조회 오류:', err);
-            res.status(500).json({ message: '서버 오류' });
-            return;
-        }
-        res.json({ message: '성공적으로 완료되었습니다.', results: results })
-    })
-});
-
-connection.connect((error) => {
-    if (error) {
-        console.error('데이터베이스 연결 실패:', error);
-        return;
-    }
-    console.log('데이터베이스에 연결되었습니다.');
-    app.listen(3001, () => {
-        console.log('Server is running on port 3001');
-    });
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
 });
