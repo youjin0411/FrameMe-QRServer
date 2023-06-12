f65935e0ca0819b489015b84a41e6bed73dd9037
Kim-yujin <w2117@e-mirim.hs.kr>
2023-06-12 23:46:37 +0900

    table

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'framemedb'
+};
+
+// create database framemedb;
+// CREATE TABLE gallery(
+//     name VARCHAR(20) NOT NULL,
+//     day VARCHAR(20) NOT NULL,
+//     time VARCHAR(20) NOT NULL,
+//     qr LONGBLOB,
+//     frame LONGBLOB NOT NULL,
+//     PRIMARY KEY(name)
+// );
